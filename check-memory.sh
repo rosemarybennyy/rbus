544f075858488bc902efb0e81d9399fff82e6004
rosemarybennyy <RoseMary_Benny@comcast.com>
2025-05-16 10:37:31 +0530

    Update check-memory.sh

--- check-memory.sh
+++ check-memory.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to the Valgrind XML file
-VALGRIND_XML="/tmp/valgrind_report/provider_valgrind_procinfo.xml"
+VALGRIND_XML="provider_valgrind_procinfo.xml"
 
 # Check if the Valgrind XML file exists
 if [ ! -f "$VALGRIND_XML" ]; then
