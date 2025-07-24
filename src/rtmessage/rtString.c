a13ca72297aa1a13fa4e9afb955507348bb421a6
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-07-24 23:45:16 +0530

    review comments

--- src/rtmessage/rtString.c
+++ src/rtmessage/rtString.c
@@ -2,27 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 
-#if 0
 uint32_t rtString_Copy(char* dstBuffer, const char* srcBuffer, uint32_t dstBufferSize)
 {
    if ((0 == dstBufferSize) || (!srcBuffer) || (!dstBuffer))
-        return 0;
-
-    int srcBufferSize = snprintf(dstBuffer, dstBufferSize, "%s", srcBuffer);
-
-    if (srcBufferSize >= dstBufferSize)
-        printf("Output was truncated.\n");
-
-    if (dstBufferSize <= srcBufferSize)
-        return dstBufferSize;
-    else
-        return srcBufferSize;
-}
-#endif
-
-uint32_t rtString_Copy(char* dstBuffer, const char* srcBuffer, uint32_t dstBufferSize)
-{
-    if ((0 == dstBufferSize) || (!srcBuffer) || (!dstBuffer))
         return 0;
 
     int srcBufferSize = snprintf(dstBuffer, dstBufferSize, "%s", srcBuffer);
