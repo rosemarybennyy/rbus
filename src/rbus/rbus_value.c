978b7a9059fa6bbf7e496a484c23da34a7784f03
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-07-25 15:52:50 +0530

    test

--- src/rbus/rbus_value.c
+++ src/rbus/rbus_value.c
@@ -312,7 +312,7 @@ char* rbusValue_ToString(rbusValue_t v, char* buf, size_t buflen)
     switch(v->type)
     {
     case RBUS_STRING:
-	rtString_Copy(p, (uint32_t) n , (char const* ) v->d.bytes->data);    
+	strncpy(p, (char const* ) v->d.bytes->data, n);    
         break;
     case RBUS_BYTES:
     {
