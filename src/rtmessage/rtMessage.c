f66c83a551413257d5c0cd184f752a583ff75876
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-07-29 14:12:21 +0530

    replaced strcpy with rtString_Copy

--- src/rtmessage/rtMessage.c
+++ src/rtmessage/rtMessage.c
@@ -432,7 +432,7 @@ rtMessage_GetSendTopic(rtMessage const m, char* topic)
   cJSON* obj = cJSON_GetObjectItem(m->json, "_topic");
   if (obj)
     //strcpy(topic, obj->valuestring);
-    rtString_Copy(topic, obj->valuestring, RTMSG_HEADER_MAX_TOPIC_LENGTH);
+    rtString_Copy(topic, obj->valuestring, strlen(obj->valuestring));
   else
     err = RT_FAIL;
   return err;
