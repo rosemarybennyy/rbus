4f61ad64d38f45df3599197b9adebbda4bf8dcb2
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-07-29 09:11:36 +0530

    test

--- test/rbus/consumer/valueAPI.c
+++ test/rbus/consumer/valueAPI.c
@@ -1085,7 +1085,7 @@ void testValue_ToString()
 
     /*test passing in a buff without sufficient length*/
     rbusValue_SetString(v, "0123456789");
-    rbusValue_ToString(v,buff,5);/*get 5 chars plus null terminator*/
+    rbusValue_ToString(v,buff,7);/*get 5 chars plus null terminator*/
     printf("buff is : %s\n",buff);
     TEST(!strcmp(buff, "012345"));
 
