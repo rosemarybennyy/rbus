9018736eff62adf7444ab2d54c1887b1463651ae
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-08-05 13:52:32 +0530

    Ignoring number of bytes read in rbusHandle_FetchGetTimeout cid:513206

--- src/rbus/rbus_handle.c
+++ src/rbus/rbus_handle.c
@@ -245,7 +245,7 @@ uint32_t rbusHandle_FetchGetTimeout(rbusHandle_t handle)
     {
         fp = fopen(fileName, "r");
         if(fp != NULL) {
-            if (fread(buf, 1, sizeof(buf), fp) > 0)
+            if (fgets(buf, sizeof(buf), fp) != NULL)
                 timeout = atoi(buf);
             fclose(fp);
         }
