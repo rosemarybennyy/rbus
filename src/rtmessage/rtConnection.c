e4fa270d605bb9461acd61046facf47a6ff310c2
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-08-05 15:10:46 +0530

    syntax error

--- src/rtmessage/rtConnection.c
+++ src/rtmessage/rtConnection.c
@@ -366,7 +366,7 @@ rtConnection_ConnectAndRegister(rtConnection con, rtTime_t* reconnect_time)
   if (fdManip < 0)
     return rtErrorFromErrno(errno);
 
-  setsockopt(con->fd, SOL_TCP, TCP_NODELAY, &i, sizeof(i))
+  setsockopt(con->fd, SOL_TCP, TCP_NODELAY, &i, sizeof(i));
 
   rtSocketStorage_ToString(&con->remote_endpoint, remote_addr, sizeof(remote_addr), &remote_port);
 
