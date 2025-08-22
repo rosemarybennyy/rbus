a97f0e4b8cdef12b5f850fb9202da260a7011f87
RoseMary_Benny@comcast.com <RoseMary_Benny@comcast.com>
2025-08-22 19:57:08 +0530

    RDKB-60980 : Addressing the possible issues reported
    Reason for change: Ignoring number of bytes read in rbus_handle.c
    Test Procedure: as per RDKB-60980
    Risks: Medium

--- src/rbus/rbus_handle.c
+++ src/rbus/rbus_handle.c
@@ -245,7 +245,7 @@ uint32_t rbusHandle_FetchGetTimeout(rbusHandle_t handle)
     {
         fp = fopen(fileName, "r");
         if(fp != NULL) {
-            if (fread(buf, 1, sizeof(buf), fp) > 0)
+            if (fgets(buf, sizeof(buf), fp) != NULL)
                 timeout = atoi(buf);
             fclose(fp);
         }
@@ -268,7 +268,7 @@ uint32_t rbusHandle_FetchSetTimeout(rbusHandle_t handle)
     {
         fp = fopen(fileName, "r");
         if(fp != NULL) {
-            if (fread(buf, 1, sizeof(buf), fp) > 0)
+            if (fgets(buf, sizeof(buf), fp) != NULL)
                 timeout = atoi(buf);
             fclose(fp);
         }
@@ -291,7 +291,7 @@ uint32_t rbusHandle_FetchGetMultiTimeout(rbusHandle_t handle)
     {
         fp = fopen(fileName, "r");
         if(fp != NULL) {
-            if (fread(buf, 1, sizeof(buf), fp) > 0)
+            if (fgets(buf, sizeof(buf), fp) != NULL)
                 timeout = atoi(buf);
             fclose(fp);
         }
@@ -314,7 +314,7 @@ uint32_t rbusHandle_FetchSetMultiTimeout(rbusHandle_t handle)
     {
         fp = fopen(fileName, "r");
         if(fp != NULL) {
-            if (fread(buf, 1, sizeof(buf), fp) > 0)
+            if (fgets(buf, sizeof(buf), fp) != NULL)
                 timeout = atoi(buf);
             fclose(fp);
         }
